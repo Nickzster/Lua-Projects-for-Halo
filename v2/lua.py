5fee7c445ce2b3046d5042e82d4cb28e812f7398
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-05-12 17:30:33 -0700

    v2 is done

--- v2/lua.py
+++ v2/lua.py
@@ -1,37 +1,150 @@
 import sys
 import os
 import shutil
-from queue import Queue  # put(value) to push, get() to remove
 
 projectName = "project"
-projectDirName = "example-one"
 sappApiVersion = "1.10.1.0"
+projectDirName = "example-one"
+sharedDir = "shared"
+sourceDir = "src"
+entryFile = "main.lua"
+
+# --DO NOT MODIFY ANYTHING UNDER THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING!!!
+
+# -- Data Structures
 
 
-def pushFullDir(dirname):
+class ListStructure:
+    def __init__(self, startingElem=None):
+        self.list = []
+        self.length = 0
+        if startingElem != None:
+            self.list.append(startingElem)
+            self.length += 1
+
+    def push(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def getLength(self):
+        return self.length
+
+
+class Queue(ListStructure):
+    def __init__(self, startingElem=None):
+        super().__init__(startingElem)
+
+    def push(self, newElem):
+        self.list.append(newElem)
+        self.length += 1
+
+    def pop(self):
+        if self.length > 0:
+            self.length -= 1
+            return self.list.pop(0)
+        else:
+            return None
+
+    def enqueue(self, newElem):
+        self.push(newElem)
+
+    def dequeue(self):
+        return self.pop()
+
+    def peek(self):
+        if self.length <= 0:
+            return None
+        return self.list[0]
+
+    def search(self, elemToFind):
+        for elem in self.list:
+            if elemToFind == elem:
+                return True
+        return False
+
+
+class Stack(ListStructure):
+    def __init__(self, startingElem=None):
+        super().__init__(startingElem)
+
+    def push(self, newElem):
+        self.list.append(newElem)
+        self.length += 1
+
+    def pop(self):
+        if self.length > 0:
+            self.length -= 1
+            return self.list.pop()
+        else:
+            return None
+
+    def search(self, elemToFind):
+        for elem in self.list:
+            if elem == elemToFind:
+                return True
+        return False
+
+    def peek(self):
+        if self.length <= 0:
+            return None
+        return self.list[self.length-1]
+
+# -- Program
+
+
+def buildDir(dirname):
     return os.path.join(os.getcwd(), dirname)
 
 
-def isTestFile(fileName):
+def isValidFile(fileName):
     modifiedFile = fileName.split(".")
     for item in modifiedFile:
         if item == "test":
+            return False
+        if item == "lua":
             return True
     return False
 
 
-def build():
+def constructFileWithDir(dirname):
     bfs = Queue()
-    bfs.put(pushFullDir(projectDirName))
+    bfs.enqueue(dirname)
+    output = ""
+    while(bfs.getLength() > 0):
+        nextItem = bfs.dequeue()
+        if os.path.isdir(nextItem) and nextItem != sharedDir:
+            cwd = os.chdir(nextItem)
+            directories = os.listdir(cwd)
+            for item in directories:
+                bfs.enqueue(buildDir(item))
+        elif isValidFile(os.path.basename(nextItem)):
+            file = open(nextItem, "r")
+            output += file.read()
+            output += "\n\n"
+            file.close()
+        else:
+            continue
+    return output
 
-    while(bfs.qsize() > 0):
-        cwd = os.chdir(bfs.get())
-        directories = os.listdir(cwd)
-        for item in directories:
-            if os.path.isdir(item):
-                bfs.put(pushFullDir(item))
-            elif isTestFile(item) == False:
-                print(item)
+
+def build():
+    projectDir = os.getcwd()
+    output = constructFileWithDir(os.path.join(
+        os.getcwd(), projectDirName, sharedDir))
+    os.chdir(projectDir)
+    output += constructFileWithDir(os.path.join(os.getcwd(),
+                                                projectDirName, sourceDir))
+    os.chdir(projectDir)
+    output += constructFileWithDir(os.path.join(os.getcwd(),
+                                                projectDirName, entryFile))
+    file = open(projectName + ".lua", "w")
+    file.write(output)
+    file.close()
 
 
 if __name__ == "__main__":
---
+++ v2/project.lua
@@ -0,0 +1,36 @@
+function UtilityThree()
+    print("Utility Three!")
+end
+
+--an important utility we expect to see in output
+function UtilityOne()
+    print("Utility One!")
+end
+
+--another important utility
+
+function UtilityTwo()
+    print("Utility Two!")
+end
+
+function moreUtilites()
+    print("Another Utility!")
+end
+
+function evenMoreUtilites()
+    print("Another Utility!")
+end
+
+function anotherUtilites()
+    print("Another Utility!")
+end
+
+function moduleOne()
+    print("Module One!")
+    myUtil = utilityOne
+end
+
+function main()
+    print("Main!")
+end
+
